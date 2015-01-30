d1a152ed4dbdeb3b6230485819954fa14ef9a131
codewalker <codewalker@hardkernel.com>
2015-01-30 14:07:31 +0900

    ODROIDC: Add Overscan function.
    Change-Id: I3e08ac21d219f37e7fe637140c7988fe97e72274

--- src/com/hardkernel/odroid/BootReceiver.java
+++ src/com/hardkernel/odroid/BootReceiver.java
@@ -8,17 +8,17 @@ import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
 
-	private final static String TAG = "ODROIDUtility";
-	
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		// TODO Auto-generated method stub
-		String action = intent.getAction();
-		if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
-			SharedPreferences pref = context.getSharedPreferences("utility", Context.MODE_PRIVATE);
-	        MainActivity.setGovernor(pref.getString("governor", "interactive"));
-	        MainActivity.setMouse(pref.getString("mouse", "right"));
-		}
-	}
+    private final static String TAG = "ODROIDUtility";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        String action = intent.getAction();
+        if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
+            SharedPreferences pref = context.getSharedPreferences("utility", Context.MODE_PRIVATE);
+            MainActivity.setGovernor(pref.getString("governor", "interactive"));
+            MainActivity.setMouse(pref.getString("mouse", "right"));
+        }
+    }
 
 }
--- src/com/hardkernel/odroid/MainActivity.java
+++ src/com/hardkernel/odroid/MainActivity.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import android.app.Activity;
 import android.content.Context;
@@ -29,11 +31,17 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     private final static String TAG = "ODROIDUtility";
     public final static String GOVERNOR_NODE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor";
+
+    public final static String WINDOW_AXIS = "/sys/class/graphics/fb0/window_axis";
+    public final static String FREE_SCALE_AXIS = "/sys/class/graphics/fb0/free_scale_axis";
+    public final static String FREE_SCALE = "/sys/class/graphics/fb0/free_scale";
+    public final static String FREE_SCALE_VALUE = "0x10001";
 
     private Spinner mSpinnerGovernor;
     private String mGovernorString;
@@ -42,6 +50,34 @@ public class MainActivity extends Activity {
     private RadioButton mRadio_right;
 
     private Spinner mSpinner_Resolution;
+    private String mResolution = "720p";
+    private static String mSystemResolution;
+
+    private int mTopValue;
+    private int mTopDelta = 0;
+    private TextView mTextViewTopValue;
+    private Button mBtnTopDecrease;
+    private Button mBtnTopIncrease;
+
+    private int mLeftValue;
+    private int mLeftDelta = 0;
+    private TextView mTextViewLeftValue;
+    private Button mBtnLeftDecrease;
+    private Button mBtnLeftIncrease;
+
+    private int mRightValue;
+    private int mRightDelta = 0;
+    private TextView mTextViewRightValue;
+    private Button mBtnRightIncrease;
+    private Button mBtnRightDecrease;
+
+    private int mBottomValue;
+    private int mBottomDelta = 0;
+    private TextView mTextViewBottomValue;
+    private Button mBtnBottomIncrease;
+    private Button mBtnBottomDecrease;
+
+    private ArrayList<Button> mBtnOverScanList;
 
     private RadioButton mRadio_portrait;
     private RadioButton mRadio_landscape;
@@ -65,6 +101,8 @@ public class MainActivity extends Activity {
             mSu = Runtime.getRuntime().exec("su");
         } catch (Exception e) {
         }
+
+        SharedPreferences pref = getSharedPreferences("utility", Context.MODE_PRIVATE);
 
         mRadio_left = (RadioButton)findViewById(R.id.radio_left);
         mRadio_right = (RadioButton)findViewById(R.id.radio_right);
@@ -107,6 +145,27 @@ public class MainActivity extends Activity {
                         mDegree = 270;
                     else if (line.contains("0"))
                         mDegree = 0;
+                }
+                if (line.contains("[hdmimode]")) {
+                    mResolution = line.substring(line.lastIndexOf("[") + 1, line.length() - 1);
+                    mSystemResolution = mResolution;
+                    Log.e(TAG, mResolution);
+                }
+                if (line.contains("[overscan.top]")) {
+                    mTopDelta = Integer.parseInt(line.substring(line.lastIndexOf("[") + 1, line.length() - 1));
+                    Log.e(TAG, "top : " + mTopDelta);
+                }
+                if (line.contains("[overscan.left]")) {
+                    mLeftDelta = Integer.parseInt(line.substring(line.lastIndexOf("[") + 1, line.length() - 1));
+                    Log.e(TAG, "left : " + mLeftDelta);
+                }
+                if (line.contains("[overscan.right]")) {
+                    mRightDelta = Integer.parseInt(line.substring(line.lastIndexOf("[") + 1, line.length() - 1));
+                    Log.e(TAG, "right : " + mRightDelta);
+                }
+                if (line.contains("[overscan.bottom]")) {
+                    mBottomDelta = Integer.parseInt(line.substring(line.lastIndexOf("[") + 1, line.length() - 1));
+                    Log.e(TAG, "bottom : " + mBottomDelta);
                 }
             }
             bufferedReader.close();
@@ -163,7 +222,6 @@ public class MainActivity extends Activity {
             @Override
             public void onNothingSelected(AdapterView<?> arg0) {
                 // TODO Auto-generated method stub
-
             }
 
         });
@@ -173,6 +231,138 @@ public class MainActivity extends Activity {
         if (mGovernorString != null) {
             mSpinnerGovernor.setSelection(mAdapterGovenor.getPosition(mGovernorString));
         }
+
+        mTextViewTopValue = (TextView)findViewById(R.id.tv_top);
+        mTextViewTopValue.setText(Integer.toString(mTopDelta));
+        mBtnTopDecrease = (Button)findViewById(R.id.btn_top_decrease);
+        mBtnTopDecrease.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if (mTopDelta == 0)
+                    return;
+                mTopDelta--;
+                if (!setOverScan())
+                    mTopDelta++;
+                mTextViewTopValue.setText(Integer.toString(mTopDelta));
+            }
+        });
+        mBtnTopIncrease = (Button)findViewById(R.id.btn_top_increase);
+        mBtnTopIncrease.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                mTopDelta++;
+                if (!setOverScan())
+                    mTopDelta--;
+                mTextViewTopValue.setText(Integer.toString(mTopDelta));
+            }
+        });
+
+        mTextViewLeftValue = (TextView)findViewById(R.id.tv_left);
+        mTextViewLeftValue.setText(Integer.toString(mLeftDelta));
+        mBtnLeftDecrease = (Button)findViewById(R.id.btn_left_decrease);
+        mBtnLeftDecrease.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if (mLeftDelta == 0)
+                    return;
+                mLeftDelta--;
+                if (!setOverScan())
+                    mLeftDelta++;
+                mTextViewLeftValue.setText(Integer.toString(mLeftDelta));
+            }
+        });
+        mBtnLeftIncrease = (Button)findViewById(R.id.btn_left_increase);
+        mBtnLeftIncrease.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if (mLeftValue == mRightValue)
+                    return;
+                mLeftDelta++;
+                if (!setOverScan())
+                    mLeftDelta--;
+                mTextViewLeftValue.setText(Integer.toString(mLeftDelta));
+            }
+        });
+
+        mTextViewRightValue = (TextView)findViewById(R.id.tv_right);
+        mTextViewRightValue.setText(Integer.toString(mRightDelta));
+        mBtnRightIncrease = (Button)findViewById(R.id.btn_right_increase);
+        mBtnRightIncrease.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if (mRightValue == mLeftValue)
+                    return;
+                mRightDelta++;
+                if (!setOverScan())
+                    mRightDelta--;
+                mTextViewRightValue.setText(Integer.toString(mRightDelta));
+            }
+        });
+        mBtnRightDecrease = (Button)findViewById(R.id.btn_right_decrease);
+        mBtnRightDecrease.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if (mRightDelta == 0)
+                    return;
+                mRightDelta--;
+                if (!setOverScan())
+                    mRightDelta++;
+                mTextViewRightValue.setText(Integer.toString(mRightDelta));
+            }
+        });
+
+        mTextViewBottomValue = (TextView)findViewById(R.id.tv_bottom);
+        mTextViewBottomValue.setText(Integer.toString(mBottomDelta));
+        mBtnBottomIncrease = (Button)findViewById(R.id.btn_bottom_increase);
+        mBtnBottomIncrease.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if (mBottomValue == mTopValue)
+                    return;
+                mBottomDelta++;
+                if (!setOverScan())
+                    mBottomDelta--;
+                mTextViewBottomValue.setText(Integer.toString(mBottomDelta));
+            }
+        });
+        mBtnBottomDecrease = (Button)findViewById(R.id.btn_bottom_decrease);
+        mBtnBottomDecrease.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if (mBottomDelta == 0)
+                    return;
+                mBottomDelta--;
+                if (!setOverScan())
+                    mBottomDelta++;
+                mTextViewBottomValue.setText(Integer.toString(mBottomDelta));
+            }
+        });
+
+        mBtnOverScanList = new ArrayList<Button>();
+        mBtnOverScanList.add(mBtnTopDecrease);
+        mBtnOverScanList.add(mBtnTopIncrease);
+        mBtnOverScanList.add(mBtnLeftDecrease);
+        mBtnOverScanList.add(mBtnLeftIncrease);
+        mBtnOverScanList.add(mBtnRightIncrease);
+        mBtnOverScanList.add(mBtnRightDecrease);
+        mBtnOverScanList.add(mBtnBottomIncrease);
+        mBtnOverScanList.add(mBtnBottomDecrease);
 
         mSpinner_Resolution = (Spinner)findViewById(R.id.spinner_resolution);
         ArrayAdapter<CharSequence> mAdapterResolution = ArrayAdapter.createFromResource(this,
@@ -186,13 +376,14 @@ public class MainActivity extends Activity {
                     int arg2, long arg3) {
                 // TODO Auto-generated method stub
                 String resolution = arg0.getItemAtPosition(arg2).toString();
-                Log.e(TAG, "Resolution = " + resolution);
-                setResolution(resolution);
+                if (mResolution.equals(resolution))
+                    return;
+                else
+                    mResolution = resolution;
 
-                SharedPreferences pref = getSharedPreferences("utility", MODE_PRIVATE);
-                SharedPreferences.Editor editor = pref.edit();
-                editor.putString("resolution", resolution);
-                editor.commit();
+                enableOverScanButtons(mSystemResolution.equals(mResolution));
+
+                Log.e(TAG, "Selected resolution = " + resolution);
             }
 
             @Override
@@ -206,11 +397,14 @@ public class MainActivity extends Activity {
         mRadio_left = (RadioButton)findViewById(R.id.radio_left);
         mRadio_right = (RadioButton)findViewById(R.id.radio_right);
 
-        SharedPreferences pref = getSharedPreferences("utility", Context.MODE_PRIVATE);
         if (pref.getString("mouse", "right").equals("right"))
             mRadio_right.setChecked(true);
         else
             mRadio_left.setChecked(true);
+
+        setOverScanRange(mResolution);
+
+        mSpinner_Resolution.setSelection(mAdapterResolution.getPosition(mResolution));
 
         Button btn = (Button)findViewById(R.id.button_mouse_apply);
         btn.setOnClickListener(new OnClickListener() {
@@ -238,6 +432,12 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
+                Log.e(TAG, "mSystemResolution = " + mSystemResolution + ", mResolution = " + mResolution);
+                if (!mSystemResolution.equals(mResolution)) {
+                    setResolution(mResolution);
+                    saveDelt2ubootenv(0, 0, 0, 0);
+                } else
+                    saveDelt2ubootenv(mLeftDelta, mTopDelta, mRightDelta, mBottomDelta);
 
                 reboot();
             }
@@ -351,6 +551,92 @@ public class MainActivity extends Activity {
         });
     }
 
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        saveDelt2ubootenv(mLeftDelta, mTopDelta, mRightDelta, mBottomDelta);
+    }
+
+    private void saveDelt2ubootenv(int left, int top, int right, int bottom) {
+        try {
+            Log.e(TAG, "saveDelt2ubootenv : " + top + " " + left + " " + right + " " + bottom);
+            OutputStream stream;
+            Process p = Runtime.getRuntime().exec("su");
+            stream = p.getOutputStream();
+            String cmd = "fw_setenv overscan_top " + top + "\n";
+            stream.write(cmd.getBytes());
+            stream.flush();
+            cmd = "fw_setenv overscan_left " + left + "\n";
+            stream.write(cmd.getBytes());
+            stream.flush();
+            cmd = "fw_setenv overscan_right " + right + "\n";
+            stream.write(cmd.getBytes());
+            stream.flush();
+            cmd = "fw_setenv overscan_bottom " + bottom;
+            stream.write(cmd.getBytes());
+            stream.flush();
+            stream.close();
+        } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        }
+    }
+
+    private void enableOverScanButtons(boolean enable) {
+        for (Button btn : mBtnOverScanList)
+            btn.setEnabled(enable);
+    }
+
+    private void setOverScanRange(String resolution) {
+        String line = "";
+        try {
+            BufferedReader r = new BufferedReader(
+                    new FileReader(FREE_SCALE_AXIS));
+            line = r.readLine();
+            r.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        String[] axis = line.split(" ");
+        mLeftValue = new Integer(axis[0]);
+        mTopValue = new Integer(axis[1]);
+        mRightValue = new Integer(axis[2]);
+        mBottomValue = new Integer(axis[3]);
+        Log.e(TAG, "Resolution = " + resolution
+                + " :  left : " + mLeftValue
+                + ", top : " + mTopValue
+                + ", right : " + mRightValue
+                + ", bottom : " + mBottomValue);
+    }
+
+    public boolean setOverScan() {
+        try {
+            String value = (mLeftValue + mLeftDelta) + " "
+                                + (mTopValue + mTopDelta) + " "
+                                + (mRightValue - mRightDelta) + " "
+                                + (mBottomValue - mBottomDelta);
+
+            Log.e(TAG, "echo " + value + " > " + WINDOW_AXIS);
+            BufferedWriter window_axis_out = new BufferedWriter(new FileWriter(WINDOW_AXIS));
+            window_axis_out.write(value);
+            window_axis_out.close();
+            BufferedWriter free_scale_out = new BufferedWriter(new FileWriter(FREE_SCALE));
+            free_scale_out.write(FREE_SCALE_VALUE);
+            free_scale_out.close();
+            return true;
+        } catch (IOException e) {
+            Log.e(TAG, "Fail OverScan");
+            enableOverScanButtons(false);
+            return false;
+        }
+    }
+
     private void reboot() {
         OutputStream stream;
         try {
@@ -402,7 +688,7 @@ public class MainActivity extends Activity {
             OutputStream stream;
             Process p = Runtime.getRuntime().exec("su");
             stream = p.getOutputStream();
-            String cmd =  "fw_setenv hdmimode " + resolution;
+            String cmd = "fw_setenv hdmimode " + resolution;
             stream.write(cmd.getBytes());
             stream.flush();
             stream.close();
