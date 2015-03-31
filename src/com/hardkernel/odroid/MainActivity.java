13b39f4800dba37917f9b6a9962b3c3f0891dc19
codewalker <codewalker@hardkernel.com>
2015-03-31 19:28:18 +0900

    ODROIDXU3: Utility: Fix edid default value.
    Change-Id: I13bb429d5292275633ae23229e6449082b634a0a

--- src/com/hardkernel/odroid/MainActivity.java
+++ src/com/hardkernel/odroid/MainActivity.java
@@ -540,7 +540,7 @@ public class MainActivity extends Activity {
                 value = "1080p30hz";
 
             writer.println("setenv hdmi_phy_res \"" + value +"\"\n");
-            writer.println("setenv edid \"1\"\n");
+            writer.println("setenv edid \"0\"\n");
             writer.println("setenv led_blink        \"1\"\n");
             writer.println("setenv bootcmd      \"movi read kernel 0 40008000;bootz 40008000\"\n");
             writer.println("setenv bootargs     \"fb_x_res=${fb_x_res} fb_y_res=${fb_y_res} vout=${vout} hdmi_phy_res=${hdmi_phy_res} edid=${edid} left=${left} right=${right} upper=${upper} lower=${lower} vsync=${vsync} hsync=${hsync} led_blink=${led_blink}\"");
