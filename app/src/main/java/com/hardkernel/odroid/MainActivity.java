8548d3840e0a4cb604240fe087208a6babafd660
codewalker <codewalker@hardkernel.com>
2015-12-01 15:30:31 +0900

    ODROID-XU3/4: Fix 800X480P60Hz.
    Change-Id: Iad5d096dac8c24cf67a1a58dd60a78f99da225f9

--- app/src/main/java/com/hardkernel/odroid/MainActivity.java
+++ app/src/main/java/com/hardkernel/odroid/MainActivity.java
@@ -621,7 +621,7 @@ public class MainActivity extends Activity {
             } else if ("576p50hz".equals(mResolution)) {
                 x_res = "720";
                 y_res = "576";
-            } else if ("800x480p54hz".equals(mResolution)) {
+            } else if ("800x480p60hz".equals(mResolution)) {
                 x_res = "800";
                 y_res = "480";
             } else if ("600p60hz".equals(mResolution)) {
@@ -664,7 +664,7 @@ public class MainActivity extends Activity {
 
             writer.println("# setenv fb_x_res \"800\"");
             writer.println("# setenv fb_y_res \"480\"");
-            writer.println("# setenv hdmi_phy_res \"800x480p54hz\"\n");
+            writer.println("# setenv hdmi_phy_res \"800x480p60hz\"\n");
 
             writer.println("# setenv fb_x_res \"800\"");
             writer.println("# setenv fb_y_res \"600\"");
