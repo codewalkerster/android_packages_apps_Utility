75e5a4a21e86844d6cff871f87257468a1133030
codewalker <codewalker@hardkernel.com>
2016-02-15 16:45:39 +0900

    ODROID-C2: Convert to Android Studio project.
    Change-Id: I223cfe1df61d85bf86017092a90065a4c6590d13

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
-        $(call all-java-files-under, src)
+        $(call all-java-files-under, java)
 
 LOCAL_PACKAGE_NAME := Utility
 
