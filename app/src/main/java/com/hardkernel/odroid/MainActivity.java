4ed96062f796605ca5a893fda67d7782bd1a1c1f
codewalker <codewalker@hardkernel.com>
2017-08-10 12:33:55 +0900

    ODROID-XU3/4: Fix because of MTP.
    Change-Id: Ia93251c33a859188ff848269ba7e930f368b6188

--- app/src/main/java/com/hardkernel/odroid/MainActivity.java
+++ app/src/main/java/com/hardkernel/odroid/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends Activity {
     public final static String DRAM_SCALING_AVAILABLE_FREQUENCY = "/sys/class/devfreq/exynos5-devfreq-mif/available_frequencies";
     public final static String DRAM_GOVERNOR_NODE = "/sys/class/devfreq/exynos5-devfreq-mif/governor";
     public final static String DRAM_FREQUENCY_NODE = "/sys/class/devfreq/exynos5-devfreq-mif/max_freq";
-    private final static String BOOT_INI = Environment.getExternalStorageDirectory() + "/boot.ini";
+    private final static String BOOT_INI = "/storage/internal/boot.ini";
 
     private Spinner mSpinnerGovernor;
     private String mGovernorString;
